90c92595a1672057c8b7deb84071473dfd7b2e0b
pandeyofficial17 <104973065+pandeyofficial17@users.noreply.github.com>
2023-12-19 11:09:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const upperSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerSet = 'abcdefghijklmnopqrstuvwxyz'
+const numberSet = '1234567890'
+const symbolSet = '~!@#$%^&*()_+/'
+
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById("upper-case")
+const lowerInput = document.getElementById("lower-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
+const passBox = document.getElementById("pass-box")
+
+
+
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
+
+const generatePassword = (password = "") =>{
+    if(upperInput.checked){
+        password+= getRandomData(upperSet)
+    }
+    if(lowerInput.checked){
+        password+= getRandomData(lowerSet)
+    }
+    if(numberInput.checked){
+        password+= getRandomData(numberSet)
+    }
+    if(symbolInput.checked){
+        password+= getRandomData(symbolSet)
+    }
+    if (password.length < totalChar.value){
+        return generatePassword(password)
+    }
+    console.log(password)
+    passBox.innerHTML = password
+}
+
+document.getElementById("btn").addEventListener("click",()=>{
+    generatePassword();
+})
+
